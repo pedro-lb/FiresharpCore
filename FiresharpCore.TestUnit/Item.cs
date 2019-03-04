2a960049f2545f28e0e65d56623a8e9b35f334de
pedrobini <pedro1451@gmail.com>
2019-03-04 10:49:52 -0300

    + Add TestUnit

---
+++ FiresharpCore.TestUnit/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace FiresharpCore.TestUnit
+{
+    internal class Item
+    {
+        public Item()
+        {
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Key { get; set; }
+    }
+}
