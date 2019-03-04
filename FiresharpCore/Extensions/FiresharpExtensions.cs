2e6626e8b54108ce6981a14d3aa3a0606531c6b2
pedrobini <pedro1451@gmail.com>
2019-03-04 10:40:16 -0300

    Changed extensions namespace

--- FiresharpCore/Extensions/FiresharpExtensions.cs
+++ FiresharpCore/Extensions/FiresharpExtensions.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace FiresharpCore.Extensions
+namespace FiresharpCore
 {
     public static class FiresharpExtensions
     {
